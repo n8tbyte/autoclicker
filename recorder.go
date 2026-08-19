ac623c8bda00bf53a5789e6090a3ff3f4993a934
n8tbyte <naruepanart1201@gmail.com>
2026-08-20 01:21:32 +0700

    Update recorder.go

--- recorder.go
+++ recorder.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -11,15 +12,23 @@ type Recorder struct {
 }
 
 func NewRecorder() *Recorder {
-	return &Recorder{
+	r := &Recorder{
 		recorded: make([]ClickPoint, 0),
 	}
+	r.Load()
+	return r
 }
 
 func (r *Recorder) IsRecording() bool {
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 	return r.isRecording
+}
+
+func (r *Recorder) Load() {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.recorded = loadPointsFromCSV()
 }
 
 func (r *Recorder) GetRecorded() []ClickPoint {
@@ -30,13 +39,26 @@ func (r *Recorder) GetRecorded() []ClickPoint {
 	return pts
 }
 
+func (r *Recorder) GetAt(index int) (ClickPoint, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	if index < 0 || index >= len(r.recorded) {
+		return ClickPoint{}, false
+	}
+	return r.recorded[index], true
+}
+
+func (r *Recorder) Len() int {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return len(r.recorded)
+}
+
 func (r *Recorder) Start() int {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	r.isRecording = true
-	r.recorded = []ClickPoint{}
-
 	r.recorded = loadPointsFromCSV()
 	return len(r.recorded)
 }
@@ -70,4 +92,39 @@ func (r *Recorder) AddPoint(x, y int, button string, delay float64) (int, bool) 
 		Delay:  delay,
 	})
 	return len(r.recorded), true
+}
+
+func (r *Recorder) UpdatePoint(index int, updateFn func(pt *ClickPoint)) error {
+	r.mu.Lock()
+	if index < 0 || index >= len(r.recorded) {
+		r.mu.Unlock()
+		return fmt.Errorf("index out of range")
+	}
+
+	updateFn(&r.recorded[index])
+	dataToSave := make([]ClickPoint, len(r.recorded))
+	copy(dataToSave, r.recorded)
+	r.mu.Unlock()
+
+	return savePointsToCSV(dataToSave)
+}
+
+func (r *Recorder) DeletePoint(index int) error {
+	r.mu.Lock()
+	if r.isRecording {
+		r.mu.Unlock()
+		return fmt.Errorf("cannot delete while recording")
+	}
+
+	if index < 0 || index >= len(r.recorded) {
+		r.mu.Unlock()
+		return fmt.Errorf("index out of range")
+	}
+
+	r.recorded = append(r.recorded[:index], r.recorded[index+1:]...)
+	dataToSave := make([]ClickPoint, len(r.recorded))
+	copy(dataToSave, r.recorded)
+	r.mu.Unlock()
+
+	return savePointsToCSV(dataToSave)
 }
