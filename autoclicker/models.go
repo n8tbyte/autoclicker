7a87ba4108f69645132e3a72ce0d9b49effad126
n8tbyte <naruepanart1201@gmail.com>
2026-08-19 14:24:53 +0700

    Update models.go

--- autoclicker/models.go
+++ autoclicker/models.go
@@ -1,8 +1,8 @@
 package main
 
 type ClickPoint struct {
-	X      int     `json:"x"`
-	Y      int     `json:"y"`
-	Button string  `json:"button"`
-	Delay  float64 `json:"delay"`
+	X      int
+	Y      int
+	Button string
+	Delay  float64
 }
