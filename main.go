5b51648e30bddcb6e353d7756a15136e7ede81cb
n8tbyte <naruepanart1201@gmail.com>
2026-08-20 01:21:31 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -12,21 +13,26 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
+var (
+	playMu    sync.Mutex
+	isPlaying bool
+	stopChan  chan struct{}
+)
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Auto Clicker " + version)
 	w.Resize(fyne.NewSize(300, 400))
 
-	statusLabel := widget.NewLabel("Press F2 to start recording")
+	statusLabel := widget.NewLabel("F1: Play/Stop | F2: Record | F3: Add Point")
 	recorder := NewRecorder()
 
-	var displayPoints []ClickPoint
 	var pointsList *widget.List
 	var refreshUIList func()
 
 	pointsList = widget.NewList(
 		func() int {
-			return len(displayPoints)
+			return recorder.Len()
 		},
 		func() fyne.CanvasObject {
 			labelNum := widget.NewLabel("")
@@ -41,6 +47,11 @@ func main() {
 			return container.NewGridWithColumns(5, labelNum, entryX, entryY, entryDelay, btnDelete)
 		},
 		func(id widget.ListItemID, o fyne.CanvasObject) {
+			pt, ok := recorder.GetAt(id)
+			if !ok {
+				return
+			}
+
 			grid := o.(*fyne.Container)
 
 			labelNum := grid.Objects[0].(*widget.Label)
@@ -54,8 +65,6 @@ func main() {
 			entryDelay.OnChanged = nil
 			btnDelete.OnTapped = nil
 
-			pt := displayPoints[id]
-
 			labelNum.SetText(fmt.Sprintf("%d", id+1))
 			entryX.SetText(fmt.Sprintf("%d", pt.X))
 			entryY.SetText(fmt.Sprintf("%d", pt.Y))
@@ -63,63 +72,46 @@ func main() {
 
 			entryX.OnChanged = func(val string) {
 				if x, err := strconv.Atoi(val); err == nil {
-					displayPoints[id].X = x
-					_ = savePointsToCSV(displayPoints)
+					_ = recorder.UpdatePoint(id, func(p *ClickPoint) { p.X = x })
 				}
 			}
 
 			entryY.OnChanged = func(val string) {
 				if y, err := strconv.Atoi(val); err == nil {
-					displayPoints[id].Y = y
-					_ = savePointsToCSV(displayPoints)
+					_ = recorder.UpdatePoint(id, func(p *ClickPoint) { p.Y = y })
 				}
 			}
 
 			entryDelay.OnChanged = func(val string) {
 				if d, err := strconv.ParseFloat(val, 64); err == nil {
-					displayPoints[id].Delay = d
-					_ = savePointsToCSV(displayPoints)
+					_ = recorder.UpdatePoint(id, func(p *ClickPoint) { p.Delay = d })
 				}
 			}
 
-			rowIndex := id
 			btnDelete.OnTapped = func() {
-				if recorder.IsRecording() {
-					updateLabel(statusLabel, "Cannot delete while recording")
+				if err := recorder.DeletePoint(id); err != nil {
+					updateLabel(statusLabel, err.Error())
 					return
 				}
-				if rowIndex >= 0 && rowIndex < len(displayPoints) {
-					displayPoints = append(displayPoints[:rowIndex], displayPoints[rowIndex+1:]...)
-					if err := savePointsToCSV(displayPoints); err != nil {
-						updateLabel(statusLabel, fmt.Sprintf("Error saving file: %v", err))
-						return
-					}
-					updateLabel(statusLabel, "Deleted point")
-					refreshUIList()
-				}
+				updateLabel(statusLabel, "Deleted point")
+				refreshUIList()
 			}
 		},
 	)
 
 	refreshUIList = func() {
 		fyne.Do(func() {
-			if recorder.IsRecording() {
-				displayPoints = recorder.GetRecorded()
-			} else {
-				displayPoints = loadPointsFromCSV()
-			}
 			pointsList.Refresh()
 		})
 	}
 
-	displayPoints = loadPointsFromCSV()
-
 	btnPlay := widget.NewButton("Play", func() {
-		go playClicks(statusLabel)
+		togglePlayStop(statusLabel, recorder)
 	})
 	btnReload := widget.NewButton("Reload", func() {
+		recorder.Load()
 		refreshUIList()
-		updateLabel(statusLabel, fmt.Sprintf("Reloaded %d points from CSV", len(displayPoints)))
+		updateLabel(statusLabel, fmt.Sprintf("Reloaded %d points from CSV", recorder.Len()))
 	})
 
 	topBox := container.NewVBox(
@@ -139,21 +131,66 @@ func main() {
 	w.ShowAndRun()
 }
 
-func playClicks(label *widget.Label) {
-	points := loadPointsFromCSV()
-	if len(points) == 0 {
-		updateLabel(label, "Warning: No points in file or file not found")
+func togglePlayStop(label *widget.Label, recorder *Recorder) {
+	playMu.Lock()
+	if isPlaying {
+		// หากกำลังเล่นอยู่ ให้ส่งสัญญาณหยุด
+		if stopChan != nil {
+			close(stopChan)
+			stopChan = nil
+		}
+		playMu.Unlock()
+		updateLabel(label, "Stopping")
 		return
 	}
-	updateLabel(label, "Play")
+
+	isPlaying = true
+	stopChan = make(chan struct{})
+	ch := stopChan
+	playMu.Unlock()
+
+	go playClicks(label, recorder, ch)
+}
+
+func playClicks(label *widget.Label, recorder *Recorder, ch chan struct{}) {
+	defer func() {
+		playMu.Lock()
+		isPlaying = false
+		stopChan = nil
+		playMu.Unlock()
+	}()
+
+	points := recorder.GetRecorded()
+	if len(points) == 0 {
+		updateLabel(label, "Warning: No points in memory or file empty")
+		return
+	}
+
+	updateLabel(label, "Playing (Press F1 to stop)")
 	for _, pt := range points {
+		// ตรวจสอบสัญญาณหยุด
+		select {
+		case <-ch:
+			updateLabel(label, "Stopped by user")
+			return
+		default:
+		}
+
 		robotgo.Move(pt.X, pt.Y)
 		robotgo.Click(pt.Button, false)
+
 		delaySec := pt.Delay
 		if delaySec <= 0 {
 			delaySec = defaultDelaySec
 		}
-		time.Sleep(time.Duration(delaySec * float64(time.Second)))
+
+		// รอตาม Delay โดยพร้อมรับสัญญาณหยุดทันที
+		select {
+		case <-ch:
+			updateLabel(label, "Stopped by user")
+			return
+		case <-time.After(time.Duration(delaySec * float64(time.Second))):
+		}
 	}
 	updateLabel(label, "Done")
 }
